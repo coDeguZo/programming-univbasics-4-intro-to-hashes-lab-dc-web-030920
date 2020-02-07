d7f4517f3c0ee5efb8d8f9b5d84262f7dc5edae2
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-07 14:26:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -49,11 +49,12 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key] == nil
-    return hash[key] = 1
+  if hash.key?(key)
+    hash[key] += 1 
   else
-    return hash[key] += 1
+    hash[key] = 1
   end
+  hash
 end
 
 
